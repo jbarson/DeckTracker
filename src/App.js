b3dfcfa56875cadb92ae94d47512bca61e89c318
Jon Barson <jbarson@gmail.com>
2017-01-31 11:10:59 -0500

    staring the initial design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './App.css';
+import {
+  Nav,
+  NavItem,
+  PageHeader,
+  Navbar
+} from 'react-bootstrap';
 
 class App extends React.Component {
+  greeting = "hi there";
   render() {
     return (
-      <p>Hi there</p>
+      <div className="container">
+        <PageHeader className="text-muted ">Deck Tracker</PageHeader>
+        <Navbar>
+          <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
+            <NavItem eventKey="1" >NavItem 1 content</NavItem>
+            <NavItem eventKey="2" >NavItem 2 content</NavItem>
+          </Nav>
+          <Nav pullRight bsStyle="tabs">
+            <NavItem eventKey="3">Login</NavItem>
+          </Nav>
+        </Navbar>
+        <div className="content">
+          {this.greeting}
+        </div>
+      </div>
     );
   }
 }
