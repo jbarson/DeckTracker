efbef13cd2086c9851ca448b38bd797e9fdbc1b2
Jon Barson <jbarson@gmail.com>
2017-02-08 10:07:38 -0500

    Added pulling accounts from firebase.

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,31 +1,55 @@
 import React from 'react';
 import {Grid, Row, Col, ListGroup, ListGroupItem} from 'react-bootstrap';
+import base from '../base';
 
+class Create extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accounts: []
+    }
+  }
+  componentDidMount() {
+    base
+      .fetch('accounts', {
+      context: this,
+      asArray: true
+    })
+      .then(data => {
+        
+        this.setState((state, props) => { return { 
+          accounts: data.map(i => i.name)
+         }});
+        
+      })
+  }
 
-const Create = () => {
-    let accounts= ["Fred", "Joe", "Lisa"]
+  render() {
     return (
-        <Grid>
-          <Row className="show-grid">
-            <Col>
-              <h2>Available Players</h2>
-              <ListGroup>
-                {accounts
-                  .map((item, index) => {
-                    return (
-                      <ListGroupItem key={index}>
-                        <input type="checkbox"/>
-                        <span>{item}</span>
-                      </ListGroupItem>
-                    )
-                  })}
-              </ListGroup>
-              <button className="btn">Create Game</button>
-              <hr/>
-            </Col>
-          </Row>
-        </Grid>
+      <Grid>
+        <Row className="show-grid">
+          <Col>
+            <h2>Available Players</h2>
+            <ListGroup>
+              {this
+                .state
+                .accounts
+                .map((item, index) => {
+                  return (
+                    <ListGroupItem key={index}>
+                      <input type="checkbox"/>
+                      <span>{item}</span>
+                    </ListGroupItem>
+                  )
+                })}
+            </ListGroup>
+            <button className="btn">Create Game</button>
+            <hr/>
+          </Col>
+        </Row>
+      </Grid>
     );
-};
+  }
+}
 
 export default Create;
