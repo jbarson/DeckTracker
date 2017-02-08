fa615cff82003fc6f0e8ae6bfd6b239d928825a4
Jon Barson <jbarson@gmail.com>
2017-02-08 09:37:22 -0500

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import {
-  PageHeader
-} from 'react-bootstrap';
 import {Link} from 'react-router';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user:null,
-      role:null,
-      accounts: ["Fred", "Joe", "Lisa"],
+      user: null,
+      role: null,
+      accounts: [
+      ],
       data: 123
     }
   }
@@ -19,18 +17,29 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <PageHeader className="text-muted ">Deck Tracker</PageHeader>
-        <button className="btn pull-right" onClick={() => this.handleLogin()}>{this.state.user
-          ? 'Log Out'
-          : 'Log In'}
-        </button>
-        <nav className="nav nav-tabs"  onSelect={this.handleSelect}>
-          <li><Link to="/create">Create Game</Link></li>
-          <li><Link to="/run">Run Game</Link></li>
-          <li><Link to="/play">Play Game</Link></li>    
+        <div className="page-header">
+          <h1>
+            <Link to="/" className="text-muted ">Deck Tracker</Link>
+          </h1>
+          <button className="btn" onClick={() => this.handleLogin()}>{this.state.user
+              ? 'Log Out'
+              : 'Log In'}
+          </button>
+        </div>
+
+        <nav className="nav nav-tabs" onSelect={this.handleSelect}>
+          <li>
+            <Link to="/create">Create Game</Link>
+          </li>
+          <li>
+            <Link to="/run">Run Game</Link>
+          </li>
+          <li>
+            <Link to="/play">Play Game</Link>
+          </li>
         </nav>
-        {React.cloneElement(this.props.children, {data: this.state.data})}
-        
+        {this.props.children}
+
       </div>
     );
   }
